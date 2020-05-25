7840a6848f8f824f63196db5474e2d6ecb622a96
Nishant Rai <nrai1185@gmail.com>
2020-05-25 20:25:53 +0530

    small change

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,35 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() => runApp(register());
+void main() => runApp(Register());
 
-class register extends StatefulWidget {
+class Register extends StatefulWidget {
   @override
-  _registerState createState() => _registerState();
+  _RegisterState createState() => _RegisterState();
 }
 
-class _registerState extends State<register> {
+class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: AnnotatedRegion<SystemUiOverlayStyle>(
-          value: SystemUiOverlayStyle.light,
-          child: Container(
+        value: SystemUiOverlayStyle.light,
+        child: Container(
           decoration: BoxDecoration(
-          gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          stops: [0.1, 0.4, 0.7, 0.9],
-          colors: [
-          Color(0xFF3594DD),
-          Color(0xFF4563DB),
-          Color(0xFF5036D5),
-          Color(0xFF5B16D0),
-          ],
-         ),
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.1, 0.4, 0.7, 0.9],
+              colors: [
+                Color(0xFF3594DD),
+                Color(0xFF4563DB),
+                Color(0xFF5036D5),
+                Color(0xFF5B16D0),
+              ],
+            ),
+          ),
         ),
-       ),
       ),
     );
   }
